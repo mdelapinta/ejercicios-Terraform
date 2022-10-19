512d1d735343996df01bf929ae4d90c982167200
mdelapinta <mdelapinta@gmail.com>
2022-10-19 17:55:50 +0200

    Intento de metr claves SSH - FALLIDO

--- main.tf
+++ main.tf
@@ -1,4 +1,13 @@
 # Fichero main.tf
 provider "aws" {
  region = "eu-west-3"
+
+ variable "ssh_key_path" {}
+
+# Recuros de clave SSH en AWS
+resource "aws_key_pair" "deployer" {
+ key_name = "deployer-key"
+ public_key = file(var.ssh_key_path)
+}
+
 }
